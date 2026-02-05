f2064102e9ad465dd3d30524c28467837c005986
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-02-05 08:10:02 +0700

    hoàn chỉnh

--- tinhtong/src/tinhtong/cong.java
+++ tinhtong/src/tinhtong/cong.java
@@ -3,7 +3,10 @@ package tinhtong;
 public class cong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 5; 
+		int b = 5;
+		int tong = a+b;
+		System.out.print("tong cua a + b la: " + tong);
 
 	}
 

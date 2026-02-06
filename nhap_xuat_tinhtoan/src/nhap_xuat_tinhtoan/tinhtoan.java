79a493eaf533cb77bc038fefb6d3b629248b4f23
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-02-06 23:52:29 +0700

    đã code xong

--- nhap_xuat_tinhtoan/src/nhap_xuat_tinhtoan/tinhtoan.java
+++ nhap_xuat_tinhtoan/src/nhap_xuat_tinhtoan/tinhtoan.java
@@ -1,5 +1,23 @@
 package nhap_xuat_tinhtoan;
 
-public class tinhtoan {
+import java.util.Scanner;
 
+public class tinhtoan {
+	public static void main(String[] args) {
+	//khai bao bien
+	double num1,num2,tong;
+	//khai bao scanner
+	Scanner banphim = new Scanner(System.in);
+	//in ra thong bao
+	System.out.print("nhập số thứ nhất: ");
+	num1 =  banphim.nextDouble();
+	System.out.print("nhập số thứ hai: ");
+	num2 =  banphim.nextDouble();
+	// tinh toan
+	tong = num1+num2;
+	//xuat
+	System.out.print("tổng của 2 số là: " + tong);
+
+
+}
 }

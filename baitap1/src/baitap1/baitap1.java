766dcc7fa5ea148263e892f14a7763a79db35d86
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-02-06 23:13:05 +0700

    hoan thien bt 1

--- baitap1/src/baitap1/baitap1.java
+++ baitap1/src/baitap1/baitap1.java
@@ -3,7 +3,18 @@ package baitap1;
 public class baitap1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//khai bao bien
+		int a = 5;
+		int b =  4;
+		//tinh toan
+		int tong = a+b;
+		int hieu = a-b;
+		float thuong = a/b;
+		//xuat ra 
+		System.out.println("tong la :"+ tong);
+		System.out.println("thuong la :"+ thuong);
+		System.out.println("hieu la :"+ hieu);
+		
 
 	}
 

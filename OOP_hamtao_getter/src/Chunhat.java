252f75c0760003b5d479d63ab6eccd0adde39e49
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-03-12 00:12:15 +0700

    xong

--- OOP_hamtao_getter/src/Chunhat.java
+++ OOP_hamtao_getter/src/Chunhat.java
@@ -1,4 +1,28 @@
 
 public class Chunhat {
+   private double dai;
+   private double rong;    
 
+public Chunhat() {    //ham tao
+	dai=10;
+	rong=5;
+	
+}
+public Chunhat(double dai,double rong) {
+	this.dai = dai;	
+}
+
+
+double getDai() {
+	return dai;
+}    //getter
+double getRong() {
+	return rong;
+}
+void setDai(double daiM) {
+	dai=daiM;
+}
+void setRong(double rongM) {
+	rong=rongM;
+}
 }
--- OOP_hamtao_getter/src/run.java
+++ OOP_hamtao_getter/src/run.java
@@ -2,8 +2,29 @@
 public class run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    Chunhat cn1 = new Chunhat();
+    Chunhat cn2 = new Chunhat(20,15);
+    Chunhat hv = new Chunhat(15,15);
+    
+    double cn1_dai= cn1.getDai();
+    double cn1_rong = cn1.getRong();
+    System.out.print("hình chữ nhật có chiều dài là : "); 
+    System.out.print(cn1_dai);
+    System.out.print("hình chữ nhật có chiều rộng là : ");
+    System.out.print(cn1_rong);
+    
+    
+    //thay doi chieu dai 
+    
+    cn1.setDai(50);
+    System.out.print("sau khi thay đổi chiều dài \n");
+    cn1_dai= cn1.getDai();
+    cn1_rong = cn1.getRong();
+    System.out.print("hình chữ nhật có chiều dài là : ");
+    System.out.print(cn1_dai);
+    System.out.print("hình chữ nhật có chiều rộng là : ");
+    System.out.print(cn1_rong);
+}
+	
 
 }

b3a1fa35a1ee7d1b5870c13864bdec034f23f0e2
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-03-19 00:30:54 +0700

    chưa xong

--- viduArraylist/src/Quynh66133140/run.java
+++ viduArraylist/src/Quynh66133140/run.java
@@ -1,10 +1,24 @@
 package Quynh66133140;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args ) {
+	// Khai bao bien danh sach 
+	ArrayList<Integer> dsSoNguyen =  new ArrayList<Integer>();
+	// a.Nhap 10 phan tu 
+	Scanner banphim = new Scanner(System.in);
+	
+	for(int i=0; i<10; i++) {
+		// NHap 1 phan tu
+		System.out.print("Moi nhap phan tu thu" + i + ": " );
+		// Cho user nhap, cat vao bien tam
+		Integer tam = banphim .nextInt();
+		// Them vao cuoi danh sach 
+		dsSoNguyen.add(tam);
+		
+	
 	}
-
 }

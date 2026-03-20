bb2fe3f38c6195caae0ff4763505ca3ccea0df01
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-03-20 21:43:54 +0700

    chưa code xong

--- viduOOP_kethua_QLNV/src/quynhqlnv/nhanvien.java
+++ viduOOP_kethua_QLNV/src/quynhqlnv/nhanvien.java
@@ -1,5 +1,45 @@
 package quynhqlnv;
 
 public class nhanvien {
-
+	  //Thuoc tinh 
+		private String maNV;
+		private String hoTen;
+		private double luongCoBan;
+		//Ham tao
+//		public NhanVien() {
+//			
+//		}
+//		public NhanVien(String maNhanVien, String tenNhanVien, double luongCoBan) {
+//			this.maNV = maNhanVien;
+//			this.hoTen = tenNhanVien;
+//			this.luongCoBan = luongCoBan;
+//		}
+		public NhanVien(String maNV, String hoTen, double luongCoBan) {
+			super();
+			this.maNV = maNV;
+			this.hoTen = hoTen;
+			this.luongCoBan = luongCoBan;
+		}
+		public NhanVien() {
+			super();
+		}
+	    public double tinhLuong() {
+	    	return luongCoBan;
+	    }
+		@Override
+		public String toString() {
+			String chuoiXuat = "";
+			chuoiXuat = chuoiXuat + "Ma NV: " + this.maNV;
+			chuoiXuat = chuoiXuat + "\nHo Ten: " + this.hoTen;
+			chuoiXuat = chuoiXuat + "\nLuong co ban: " + this.luongCoBan;
+			return chuoiXuat;
+//			return "NhanVien [maNV=" + maNV + ", hoTen=" + hoTen + ", luongCoBan=" + luongCoBan + "]";
+		}
+	    public void hienThiThongTin() {
+	    	System.out.println("Ma NV: " + maNV);
+	    	System.out.println("Ho ten: " + hoTen);
+	    	System.out.println("Luong co ban: " + luongCoBan);
+	    }
+	   
+	    
 }

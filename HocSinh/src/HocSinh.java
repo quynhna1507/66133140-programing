2b491950c81f47d7dfa9ebcff39758c1a25db252
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-03-26 09:15:29 +0700

    CHUA CODE XONG

--- HocSinh/src/HocSinh.java
+++ HocSinh/src/HocSinh.java
@@ -1,4 +1,37 @@
 
 public class HocSinh {
+	private String tenHS;
+	private int tuoiHS;
+	private String lopHS;
+	public String getTenHS() {
+		return tenHS;
+	}
+	public void setTenHS(String tenHS) {
+		this.tenHS = tenHS;
+	}
+	public int getTuoiHS() {
+		return tuoiHS;
+	}
+	public void setTuoiHS(int tuoiHS) {
+		this.tuoiHS = tuoiHS;
+	}
+	public String getLopHS() {
+		return lopHS;
+	}
+	public void setLopHS(String lopHS) {
+		this.lopHS = lopHS;
+	}
+	
+public HocSinh(String tenHS,int tuoiHS,String lopHS) {
+	this.tenHS= tenHS;
+	this.lopHS= lopHS;
+	this.tuoiHS = tuoiHS;
+	
+}
+@Override
+public String toString() {
+	return "HocSinh [tenHS=" + tenHS + ", tuoiHS=" + tuoiHS + ", lopHS=" + lopHS + "]";
+}
+
 
 }

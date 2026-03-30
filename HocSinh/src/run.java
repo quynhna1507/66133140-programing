ecc5535bb414b31deb29d8709038b0330194f48b
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-03-30 23:22:08 +0700

    chua code xong

---
+++ HocSinh/src/run.java
@@ -0,0 +1,15 @@
+
+
+public class run {
+
+	public static void main(String[] args) {
+	//tao moi 2 hs 
+		HocSinh hs1 = new HocSinh();
+		String tenHS1 = "Trần Văn A";
+		hs1.setTenHS(tenHS1);
+		
+		
+
+	}
+
+}

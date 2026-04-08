e0ed429e3616f08841245966e1ced9a08022053a
anhquynhn031-afk <anhquynhn031@gmail.com>
2026-04-08 23:13:31 +0700

    đã code

--- SinhVienNTU/src/quynh66133140/run.java
+++ SinhVienNTU/src/quynh66133140/run.java
@@ -2,4 +2,15 @@ package quynh66133140;
 
 public class run {
 
+	public static void main(String[] args) {
+			// In ra tên trường của sinh viên NTU
+			System.out .println(svNTU. TEN_TRUONG) ;
+			// In ra Hi, ...
+			svNTU.sayHi() ;
+			svNTU sv1 = new svNTU("Nguyễn Văn A", "KHMT-1");
+			svNTU sv2 = new svNTU("Nguyễn Anh Quỳnh", "cntt-15") ;
+			sv1.sayHello(); sv2.sayHello() ;
+
+	}
+
 }
--- SinhVienNTU/src/quynh66133140/svNTU.java
+++ SinhVienNTU/src/quynh66133140/svNTU.java
@@ -1,5 +1,20 @@
 package quynh66133140;
 
 public class svNTU {
-
+	private String hoVaTen; 
+	private String lop;
+	public svNTU(String hoVaTen, String lop) {
+	super () ;
+	this.hoVaTen = hoVaTen;
+	this.lop = lop;
+	}
+	public static String TEN_TRUONG= "ĐẠI HỌC NHA TRANG" ;
+	public static void sayHi() {
+	System.out .println("Hi, I'm an NTU Student") ;
+	}
+		public void sayHello() {
+		System.out.println ("HELL00000, I'm an NTU Student: \nTên là" + hoVaTen + " \nLớp" +lop);
+		
+		// getter, setter, ..
+		}
 }
